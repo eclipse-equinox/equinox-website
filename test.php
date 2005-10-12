f1b3aa83afd8632063f6b126beb7380d25bbe38c
jeff <jeff>
2005-10-12 14:06:41 +0000

    *** empty log message ***

---
+++ test.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Equinox - An OSGi  Framework</title>
+<link rel="stylesheet" href="http://dev.eclipse.org/default_style.css" type="text/css">
+<link rel="stylesheet" href="equinox.css" type="text/css">
+</head>
+<body>
+
+<!--#include file="nav-include.html"-->
+<?php include("nav-include.html") ?>
+
+<p class=bar>Mission Statement</p>
+<p>The goal of the Equinox project is to be a first class OSGi
+  community and foster the vision of Eclipse as a landscape of bundles. As part
+  of this, it is responsible for developing and delivering the OSGi framework
+  implementation
+  used for all
+  of
+  Eclipse. In addition. the
+  project
+  is open to:</p>
+<ul>
+  <li>Implementation of all aspects of the OSGi specification (including the MEG
+  and VEG work) </li>
+  <li>    Investigation and research related to future versions of OSGi specifications
+  and related runtime issues </li>
+  <li>Development of non-standard infrastructure deemed to be essential
+  to the running and management of OSGi-based systems </li>
+  <li>Implementation of key framework services
+      and extensions needed for running Eclipse (e.g., the Eclipse Adaptor, Extension
+  registry) and deemed generally useful to people using OSGi.  </li>
+</ul>
+  As a peer of the Platform, JDT and PDE projects, the Equinox OSGi code is
+  managed by the Eclipse PMC and ships with the Eclipse project
+major releases. The various other bundles developed here may ship independently
+  and on different schedules. 
+  <p>Did you come here expecting to find the Equinox Technology project? It has
+  been transitioned. See the <a href="documents/transition.html">transition documentation</a> for more details.</p>
+  <p class=bar>What is Equinox</p>
+  <p> In short, Equinox is an implementation of the OSGi R4 framework specification
+    and a set of bundles that implement the optional OSGi services. You can get
+    and use these individually or together.</p>
+  <p>See the <a href="quickstart.html">Quick Start Guide</a> for how to get up and running with the standalone
+    Equinox OSGi framework. </p>
+  <p>See the <a href="downloads.html">downloads</a> page for more information on acquiring bundles for use
+    with OSGi.</p>
+  <p class=bar>What's New</p>
+<p>
+  <b>Oct 2</b> - <img src="http://www.eclipse.org/images/new.gif" width="31" height="14"> Track
+  the transition details in <a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=110486">bugzilla</a>.</p>
+<p> <b>Sept 23</b> - <img src="http://www.eclipse.org/images/new.gif" width="31" height="14"> Equinox
+transition approved!! The infrastructure transition should begin presently.</p>
+<p><b>Sept 16</b> - <img src="http://www.eclipse.org/images/new.gif" width="31" height="14"> Transition
+    call scheduled for September 23, 2005 at 1800UTC (1400 EST).</p>
+<p><b>Sept 5</b> - <img src="http://www.eclipse.org/images/new.gif" width="31" height="14"> <a href="documents/transition.html">Proposal</a> to
+  transition Equinox from the Technology PMC to the Eclipse PMC and have it be
+  the official home of OSGi at Eclipse.</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
