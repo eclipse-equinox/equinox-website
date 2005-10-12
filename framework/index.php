dde6164321752c66b2472e94ffd040dd08d87954
jeff <jeff>
2005-10-12 14:20:52 +0000

    *** empty log message ***

---
+++ framework/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Equinox OSGi framework</title>
+<link rel="stylesheet" href="http://dev.eclipse.org/default_style.css" type="text/css">
+<link rel="stylesheet" href="../equinox.css" type="text/css">
+</head>
+<body>
+<?php include("nav-include.html") ?>
+<p class=bar>Mission Statement</p>
+<p><strong>Note: The role of Equinox is currently in flux. See the <a href="../documents/transition.html">Transition
+  Proposal</a>  for more details.</strong></p>
+</body>
+</html>
