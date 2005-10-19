e6fd4c20456eea6e9d1e730833af4fc715ec6922
jeff <jeff>
2005-10-19 13:27:01 +0000

    *** empty log message ***

--- bundles/index.php
+++ bundles/index.php
@@ -15,8 +15,69 @@
   team defines and produces bundles and services that are of general utility
   to OSGi systems and programmers. For example, the Bundles team is responsible
   for the Extenstion registry used throughout Eclipse.</p>
-    <p class=bar>What's New</p>
-    <p class=bar>Committers</p>
+<p class=bar>Bundle Projects</p>
+<p>The Bundles component consists of several &quot;projects&quot; in the Eclipse
+  CVS repository (see the <a href="resources.php">resources</a> page for information
+  on repository location and access). In general the name of the project matches
+  the symbolic name of the resultant bundle.</p>
+<table BORDER=0 CELLSPACING=5 CELLPADDING=2 WIDTH="100%" >
+  <tr>
+    <td width="2%" align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+    <td width="98%"><strong>OSGi Services API (<a href="http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.osgi.services">org.eclipse.osgi.services</a>)</strong><br>
+      This bundle contains the set of OSGi specified service APIs. It is included
+        here as a convenience for people developing or deploying service implementations
+        independent of their specification.</td>
+  </tr>
+  <tr>
+    <td align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+    <td><strong>OSGi Utilities (<a href="http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.osgi.services">org.eclipse.osgi.util</a>)</strong><br>
+    This bundle contains the set of OSGi specified utilities (e.g., ServiceTracker).
+    It is included here as a convenience..</td>
+  </tr>
+  <tr>
+    <td align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+    <td><b>Extension Registry  (<a href="http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.equinox.registry">org.eclipse.equinox.registry</a>)</b><br>
+      <font color="#FF0000">Coming soon...</font> The Eclipse extension registry allows bundles to communicate
+        and cooperate using declarative markup and lazy instantiation. The extension
+        mechanism
+        is similar to OSGi services and declarative services but has some different
+        contractual and management characteristics.<font color="#FF0000"> (Currently
+        this code is part of the Eclipse runtime but it is being factored out
+        to be standalone)</font></td>
+  </tr>
+  <tr>
+    <td align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+    <td><b>Declarative Services (<a href="http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.equinox.scr">org.eclipse.equinox.scr</a>)</b><br>
+        <font color="#FF0000">Coming soon...</font> An implementation of the
+        OSGi R4 Declarative Services specification.</td>
+  </tr>
+  <tr>
+    <td align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+    <td><b>Log Service (<a href="http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.equinox.log">org.eclipse.equinox.log</a>)</b><br>
+        <font color="#FF0000">Coming soon...</font> An implementation of the
+    OSGi R4 Log service.</td>
+  </tr>
+  <tr>
+    <td align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+    <td><b>Config Admin (<a href="http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.equinox.config">org.eclipse.equinox.config</a>)</b><br>
+        <font color="#FF0000">Coming soon...</font> An implementation of the
+    OSGi R4 Configuration Admin service.</td>
+  </tr>
+  <tr>
+    <td align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+    <td><b>HTTP Service (<a href="http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.equinox.http">org.eclipse.equinox.http</a>)</b><br>
+      <font color="#FF0000">Coming soon...</font> An implementation of the OSGi
+      R4 HTTP service.</td>
+  </tr>
+</table>
+<p>The Framework team typically develops using Eclipse itself so getting and
+  managing the projects is quite straightforward. Simply start Eclipse, and use <strong>Import &gt; CVS &gt; Projects
+  from CVS</strong>. Enter the repo access information and choose the projects
+  you want to load into the workspace. </p>
+<p>If you are working without Eclipse (gads! :-) then use your favorite CVS client
+  and check out the relevant projects.</p>
+<p class=bar>What's New</p>
+<p class=bar>Committers</p>
       <p>The following people are committers on the Equinox bundles component:</p>
       <ul>
         <li>Jennifer Fogell, IBM PvC</li>
--- framework/index.php
+++ framework/index.php
@@ -13,7 +13,43 @@
   to the OSGi Core Framework R4 specification. In addition, the Framework component
   produces launchers, bootstrap infrastructure and application models that facilitate
   the use of Equinox OSGi in end-user product scenarios.</p>
-    <p class=bar>What's New</p>
+
+<p class=bar>Framework Projects</p>
+<p>The Framework component consists of several &quot;projects&quot; in the Eclipse CVS
+  repository (see the <a href="resources.php">resources</a> page for information on repository location and
+  access).</p>
+<table BORDER=0 CELLSPACING=5 CELLPADDING=2 WIDTH="100%" >
+  <tr>
+    <td width="2%" align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+    <td width="98%"><strong>OSGi R4 Framework (<a href="http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.osgi/">org.eclipse.osgi</a>)</strong><br>
+    The main framework project. This includes a set of adaptors and enough
+      function to make a standalone OSGi framework. When built this project produces
+    org.eclispe.osgi.jar.</td>
+  </tr>
+  <tr>
+    <td align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+    <td><b>Boot support (org.eclipse.equinox.boot)</b><br>
+    This is the Eclipse startup.jar that helps setup the framework classloader
+      etc. <font color="#FF0000">Note: this code has yet to be moved. It is currently
+      in the <a href="http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.platform/">org.eclipse.platform</a> project.</font> See <a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=113069">bug
+      113069</a> for more details.</td>
+  </tr>
+  <tr>
+    <td align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+    <td><b>Native Launcher (<a href="http://dev.eclipse.org/viewcvs/index.cgi/platform-launcher/">platform-launcher</a>)</b><br>
+    The launcher is the native executable that runs startup.jar and thus
+      the framework. It is also responsible for putting up the splash screen
+      etc. The launcher is written in C and currently supports a wide range of
+    operating systems, window systems and processor architectures.</td>
+  </tr>
+</table>
+<p>The Framework team typically develops using Eclipse itself so getting and
+  managing the projects is quite straightforward. Simply start Eclipse, and use
+  <strong>Import &gt; CVS &gt; Projects from CVS</strong>. Enter the repo access
+information and choose the projects you want to load into the workspace. </p>
+<p>If you are working without Eclipse (gads! :-) then use your favorite CVS client
+  and check out the relevant projects.</p>
+<p class=bar>What's New</p>
 
 <p class=bar>Committers</p>
       <p>The following people are committers on the Equinox Framework component:</p>
