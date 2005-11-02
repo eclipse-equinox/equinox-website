d94face819412e4ca21d589fce1f509291eee2a7
jeff <jeff>
2005-11-02 02:15:55 +0000

    *** empty log message ***

--- incubator/server/proposal.php
+++ incubator/server/proposal.php
@@ -14,19 +14,14 @@
 
 <p>Increasingly, developers are thinking about how they might use
 Eclipse's powerful component model and extension mechanisms in
-server-side environments. For example, some are motivated by the possibility
-  of a shared runtime that
+server-side environments. The possibility of a shared runtime that
 allows the re-use of components between client and server motivates
-some while others see great promise in being able to safely modify the
-software running on a live system. </p>
-<p>Running Eclipse on the server will take a different
-    mindset
-    (and a fair bit
+some. Others see great promise in being able to safely modify the
+components and configuration of software running in a live server environment. </p>
+<p>Running Eclipse on the server will take a different mindset (and a fair bit
   of work) as it questions some of the design and implementation assumptions
   made and adds a number of additional constraints on Eclipse's running environment.
-  For example, in a
-  server-side
-  environment: </p>
+  For example, in a server-side environment: </p>
 <ul>
 	<li>You can make few assumptions on the control of global resources</li>
 	<li>Your lifecycle is controlled externally</li>
@@ -35,12 +30,11 @@ software running on a live system. </p>
 	server elements</li>
 </ul>
 
-<p>The  key goal of this incubator is to bring existing work together to
-make exploration practical. The intent is to help foster solutions to issues
+<p>The key goal of this incubator is to bring existing work together to
+make exploration practical. The intent is to help foster solutions to the issues
   relating to integrating Eclipse with server-side technology. The initial
 emphasis is on J2EE implementations but more specifically the following issues
-  are to
-be addressed (this list will evolve):</p>
+  are to be addressed (this list will evolve):</p>
 <ul>
 	<li>Eclipse changes for server-side operation</li>
 	<li>Container specific launchers</li>
@@ -70,23 +64,19 @@ that in many cases, J2EE app servers set these singletons before
 Equinox starts. This makes it dangerous or impossible for Eclipse
 to set them. The third item prevents Equinox from being
 instantiated multiple times. </p>
-<p>Tackling these
-    issues will result in
-    fairly large,
-  cross-cutting changes in Equinox that are very difficult to
-  resolve with individual patches. The goal for this area is to find a way to
-  make these changes without compromising compatibility with existing Eclipse
-  applications. Any
-changes will trace the HEAD very closely to allow integration to
-proceed. Ideally these changes would occur in the 3.2 timeframe.</p>
+<p>Tackling these issues will result in fairly large,
+cross-cutting changes in Equinox that are very difficult to
+resolve with individual patches. The goal for this area is to find a way to
+make these changes without compromising compatibility with existing Eclipse
+applications. Any changes will trace the HEAD very closely to facilitate integration.
+Ideally these changes would occur in the 3.2 timeframe.</p>
 <p class="bar">Container Specific Launchers</p>
 
-<p>One way to think of the Eclipse IDE and RCP applications is that they
-are started by the native (executable) launcher. For server-side environments,
+<p>One way to think of the Eclipse IDE and RCP applications (in general) is that they
+are started by the native executable launcher. For server-side environments,
 Eclipse must be launched in a different manner. Initially, this area of the incubator
-  will focus on a launcher appropriate for embedding in a servlet container;
-  however, other
-launchers will be added over time. After the issues for this first environment
+will focus on a launcher appropriate for embedding in a servlet container;
+however, other launchers will be added over time. After the issues for this first environment
 are addressed, other scenarios should be far easier to tackle.</p>
 
 <p>
@@ -95,7 +85,7 @@ launcher will be part of the initial contribution for the incubator.
 </p>
 
 <p class="bar">Application Server Integration</p>
-The Geronimo community have expressed some interest in OSGi. This incubator intends
+The Geronimo community has expressed some interest in OSGi. This incubator intends
 to investigate what it means to integrate OSGi and Geronimo as well as any changes
 to Equinox that might be required to support Geronimo. Integration with other
 application servers is also of interest.
