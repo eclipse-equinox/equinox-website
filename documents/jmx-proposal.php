a97a41f4f70559225a210ce433a25508da08984f
jeff <jeff>
2006-03-13 22:38:57 +0000

    *** empty log message ***

---
+++ documents/jmx-proposal.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta
+	http-equiv="Content-Type"
+	content="text/html; charset=iso-8859-1" />
+<title>The Equinox Project - Remote management using JMX</title>
+<link rel="stylesheet" href="http://eclipse.org/default_style.css" type="text/css" />
+<link rel="stylesheet" href="../equinox.css" type="text/css" />
+</head>
+<body>
+<?php include("nav-include.html") ?>
+<p class=bar>Remote management using JMX</p>
+<p>The goal of Eclipse to become a platform to produce flexible and dynamic applications,
+while realized, does not currently allow for application
+management from a remote location.  This incubator carries the very modest scope
+  of providing an extensible framework in support of running a JMX
+  server in an instance of OSGi. The framework then allows users to contribute
+  providers
+which in turn expose managed objects (i.e., MBeans). 
+<p>As part of this work we intend
+    to implement a rudimentary Eclipse client user interface to demonstrate interaction
+  with local or remote MBeans.
+<p class="bar">Interested Parties</p>
+<ul>
+  <li>Robert Connell (UNB)</li>
+  <li>DJ Houghton (IBM)</li>
+  <li>Simon Kaegi (Cognos)</li>
+  <li>Jeff McAffer (IBM)</li>
+</ul>
+<p>Please direct comments or questions to <a
+	href="mailto:equinox-dev@eclipse.org">equinox-dev@eclipse.org</a>.</p>
+</body>
+</html>
