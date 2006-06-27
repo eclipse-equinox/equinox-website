c50d7a9c4f7098eedf6329d409b610fa44fa7785
dj <dj>
2006-06-27 16:07:46 +0000

    Updated web page to use new format.

--- bundles/planning/3.2/plan.php
+++ bundles/planning/3.2/plan.php
@@ -1,15 +1,33 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<html>
-<head>
-<title>Eclipse Platform/Core</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<link rel="stylesheet" href="http://dev.eclipse.org/default_style.css" type="text/css">
-<link rel="stylesheet" href="http://eclipse.org/equinox/equinox.css" type="text/css">
-</head>
-<body>
-<?php include("../../nav-include.html") ?>
-<p class=bar>Planning</p>
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 
+	#*****************************************************************************
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Equinox Bundles - 3.2 Planning";
+	$pageKeywords	= "equinox, osgi, framework, runtime, bundles, planning";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the markers!	
+ob_start();
+?>		
+
+	<div id="midcolumn">
+		<h1><?= $pageTitle ?></h1>
+
+		<div class="homeitem3col">
+			<h3>Planning</h3>
 <table BORDER=0 CELLPADDING=2 WIDTH="100%" >
   <tr>
     <td ALIGN=RIGHT VALIGN=TOP><img SRC="../../../images/Adarrow.gif" BORDER=0 height=16 width=16></td>
@@ -41,5 +59,30 @@
     </td>
   </tr>
 </table>
-</body>
-</html>
+		</div>
+	</div>
+
+	<div id="rightcolumn">
+		<div class="sideitem">
+			<h6>Equinox links</h6>
+			<ul>
+				<li><a href="http://www.eclipse.org/equinox">home</a></li>
+				<li><a href="http://www.eclipse.org/equinox/framework">framework</a></li>
+				<li><a href="http://www.eclipse.org/equinox/bundles">bundles</a></li>
+				<li><a href="http://www.eclipse.org/equinox/incubator">incubator</a></li>
+				<li><a href="http://wiki.eclipse.org/index.php/Equinox">wiki</a></li>
+				<li><a href="http://www.eclipse.org/equinox/documents">documents</a></li>
+				<li><a href="http://download.eclipse.org/eclipse/equinox">downloads</a></li>
+				<li><a href="http://www.eclipse.org/equinox/resources.php">resources</a></li>
+				<li><a href="http://www.eclipse.org/equinox/faq.php">faq</a></li>
+			</ul>
+		</div>
+	</div>
+
+<?php
+	$html = ob_get_contents();
+	ob_end_clean();
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
