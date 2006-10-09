810ef25388aed26123c4560ce0df7a4f631aa3fe
jeff <jeff>
2006-10-09 13:39:44 +0000

    *** empty log message ***

--- documents/coding.php
+++ documents/coding.php
@@ -1,13 +1,30 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<title>Equinox Coding Practices</title>
-<link rel="stylesheet" title="default" href="http://eclipse.org/default_style.css" type="text/css">
-<link rel="stylesheet" title="default" href="../equinox.css" type="text/css">
-</head>
-<body>
-<?php include("nav-include.html") ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Equinox Coding Practices";
+	$pageKeywords	= "equinox, osgi, framework, runtime, documents, coding, practices";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the markers!	
+ob_start();
+?>		
+	<div id="midcolumn">
+		<h1><?= $pageTitle ?></h1>
+
 <p class="bar">Coding Practices</p>
 <p>Equinox covers a reasonably large code base. To ensure coherence
   and consistency, the team follows a number of coding conventions and practices.
@@ -112,5 +129,19 @@
   <li> Make sure if the message accepts a parameter then you pass one in</li>
 </ul>
 <p>&nbsp;</p>
-</body>
-</html>
+
+	</div>
+
+<?php
+	include $_SERVER['DOCUMENT_ROOT'] . "/equinox/global-links.html";
+	include $_SERVER['DOCUMENT_ROOT'] . "/equinox/documents/component-links.html";
+	include "dir-links.html";
+?>
+
+<?php
+	$html = ob_get_contents();
+	ob_end_clean();
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
