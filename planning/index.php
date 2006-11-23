89fc6704637c693610a7e30471a4b453614a0d0a
dj <dj>
2006-11-23 20:28:39 +0000

    page reorg for planning

---
+++ planning/index.php
@@ -0,0 +1,86 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Equinox Planning";
+	$pageKeywords	= "equinox, osgi, framework, runtime, bundles, planning";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the markers!	
+ob_start();
+?>		
+
+	<div id="midcolumn">
+		<h1><?= $pageTitle ?></h1>
+
+<table BORDER=0 CELLPADDING=2 WIDTH="100%" >
+
+	<tr>
+		<td WIDTH="2%" ALIGN=RIGHT VALIGN=TOP><img SRC="../../images/Adarrow.gif" BORDER=0 height=16 width=16></td>
+		<td><b>Bundles</b>
+			<ul>
+				<li>Check out the <a href="../bundles/planning">plan</a> for the Equinox Bundles component.</li>
+			</ul>
+			<p>&nbsp;</p>
+		</td>
+	</tr>
+
+	<tr>
+		<td WIDTH="2%" ALIGN=RIGHT VALIGN=TOP><img SRC="../../images/Adarrow.gif" BORDER=0 height=16 width=16></td>
+		<td><b>Framework</b>
+			<ul>
+				<li>Check out the <a href="../framework/planning">plan</a> for the Equinox Framework component.</li>
+			</ul>
+			<p>&nbsp;</p>
+		</td>
+	</tr>
+
+	<tr>
+		<td WIDTH="2%" ALIGN=RIGHT VALIGN=TOP><img SRC="../../images/Adarrow.gif" BORDER=0 height=16 width=16></td>
+		<td><b>Incubator</b>
+			<ul>
+				<li>Check out the plan for the Equinox Incubator component. <em>(TBD)</em></li>
+			</ul>
+			<p>&nbsp;</p>
+		</td>
+	</tr>
+
+	<tr>
+		<td WIDTH="2%" ALIGN=RIGHT VALIGN=TOP><img SRC="../../images/Adarrow.gif" BORDER=0 height=16 width=16></td>
+		<td><b>Server</b>
+			<ul>
+				<li>Check out the plan for the Equinox Server component. <em>(TBD)</em></li>
+			</ul>
+			<p>&nbsp;</p>
+		</td>
+	</tr>
+
+</table>
+<p>&nbsp;</p>
+	</div>
+
+<?php
+	include $_SERVER['DOCUMENT_ROOT'] . "/equinox/global-links.html";
+	if (file_exists("dir-links.html")) {include "dir-links.html";}
+?>
+
+<?php
+	$html = ob_get_contents();
+	ob_end_clean();
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
