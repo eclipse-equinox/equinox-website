bcdeb0ccf49a8181d3630edb86030a378cfc1c05
jeff <jeff>
2006-11-24 17:02:01 +0000

    add more configuraiton info and general update

--- documents/quickstart.php
+++ documents/quickstart.php
@@ -30,11 +30,21 @@ ob_start();
 <p>The Equinox OSGi framework implementation forms the underpinnings of the Eclipse
 RCP and IDE platforms but it is in fact a fully standalone OSGi implementation.
   To run Equinox OSGi on its own either download the org.eclipse.osgi JAR from
-the download site or look in your Eclipse 3.1 or 3.2 install for a file like &lt;install
-location&gt;/eclipse/plugins/org.eclipse.osgi_3.1.0.jar. Once you have the Equinox
-framework, use the following command line:</p>
-<pre>  java -jar org.eclipse.osgi_3.1.0.jar -console</pre>
-<p>  You will then see an osgi&gt; prompt. Type '?' at the prompt for command help.</p>
+the <a href="http://download.eclipse.org/eclipse/equinox/">download site</a> or look in your Eclipse install for a file like &lt;install
+location&gt;/eclipse/plugins/org.eclipse.osgi_3.2.0_xxx.jar. Once you have the Equinox
+framework JAR, use the following command line:</p>
+<pre>  java -jar org.eclipse.osgi_3.2.0.jar -console</pre>
+<p>Once this is running you will see an osgi&gt; prompt. This is the OSGi console waiting for you to type commands.  
+Type '?' at the prompt for command help.  The most interesting commands for getting started are:
+<ul>
+<li>install &lt;bundle URL&gt; - Installs the bundle from the given URL</li>
+<li>start &lt;bundle # or bundle name&gt; - Starts the bundle with the given numeric or symbolic id</li>
+<li>stop &lt;bundle # or bundle name&gt; - Stops the bundle with the given numeric or symbolic id</li>
+<li>ss - Reports a summary status of all installed bundles</li>
+<li>diag &lt;bundle # or bundle name&gt; - Reports any resolution problems for the bundle with the given numeric or symbolic id</li>
+</li>
+</p>
+
 <p>If you don't have Eclipse and just want OSGi, click <a href="http://download.eclipse.org/eclipse/equinox/">here</a> to get the JAR from the 
 Equinox download site. </p>
 <p class=bar>Configurations and all that...</p>
@@ -43,53 +53,64 @@ Equinox download site. </p>
   and run a set of bundles when it is started. You do this every time you run
 Eclipse the IDE. Here's how it works and how you can use it in your situation.</p>
 <p>Say you have bundles B1.jar and B2.jar that you want to have installed and
-  started on startup. As with other OSGi implementations, you can run the framework
+  started when Equinox is run. As with other OSGi implementations, you can run the framework
   (see
 above) and install and start B1 and B2 using the console. This is a manual process
-  but you need only do it the first time you setup Equinox.  To do something
+  but you need only do it the first time you run Equinox (OSGi frameworks remember the installed and
+  started bundles from run to run).  To do something
     completely automated, create a <em>configuration</em> that
     lists B1 and B2 as bundles to be installed and started. Set up
   something like</p>
 <pre>somedir/
-  org.eclipse.osgi_3.1.0.jar
+  org.eclipse.osgi_3.2.0.jar
   B1.jar
   B2.jar
   configuration/
     config.ini</pre>
 <p>Where config.ini is a Java properties file that contains the following line.
-  Note
-that the osgi.bundles property is quite powerful. See the doc for details. </p>
+  Note that the osgi.bundles property is quite powerful. See the doc for details. </p>
 <pre>  osgi.bundles=B1.jar@start, B2.jar@start
   eclipse.ignoreApp=true
 </pre>
 <p>When Equinox is started using the command line above, B1 and B2
   are installed and started. If you want to specify a different configuration,
-  just
-  add -configuration &lt;location&gt; to the command line. Notice that since
+  just add -configuration &lt;location&gt; to the command line. Notice that since
   you are just running your bundles, we added a line to tell Equinox to skip
   trying to start an Eclipse application. This setting is not critical but without
   it a scary but otherwise inoccuous message appears in your log. For more information
     on the set of command-line args and system properties available
     to set things
-  up, see (note that several options have been added to 3.2 and that doc is currently
-  only available in the full Eclipse SDK downloads).</p>
+  up, see </p>
 <blockquote>
-  <p>  <a href="http://help.eclipse.org/help31/topic/org.eclipse.platform.doc.isv/reference/misc/runtime-options.html">http://help.eclipse.org/help31/topic/org.eclipse.platform.doc.isv/reference/misc/runtime-options.html</a></br>
-  <a href="http://help.eclipse.org/help31/topic/org.eclipse.platform.doc.isv/reference/misc/index.html">http://help.eclipse.org/help31/topic/org.eclipse.platform.doc.isv/reference/misc/index.html</a></p>
+  <p>
+    <a href="http://help.eclipse.org/help32/topic/org.eclipse.platform.doc.isv/reference/misc/index.html">http://help.eclipse.org/help32/topic/org.eclipse.platform.doc.isv/reference/misc/index.html</a></br>
+    <a href="http://help.eclipse.org/help32/topic/org.eclipse.platform.doc.isv/reference/misc/runtime-options.html">http://help.eclipse.org/help32/topic/org.eclipse.platform.doc.isv/reference/misc/runtime-options.html</a>
+  </p>
 </blockquote>
 <p>Now, if you want to make things even easier, you can add in a few Eclipse
-  bundles (i.e., org.eclipse.core.runtime and org.eclipse.update.configurator).
-  This gives you an application model, automatic bundle discovery/install, extension
-  registry, ... Standard Eclipse tooling configurations use this approach. See
-  the Eclipse plug-in list for plug-ins that may be useful to you.</p>
-<p>For those who are interested in some of the Eclipse extensions to OSGi, see</p>
+  bundles (i.e., org.eclipse.equinox.common and org.eclipse.update.configurator).
+  This gives you automatic bundle discovery/install.  Add these bundles on the osgi.bundles
+  list in your config.ini as follows:</p>
+<pre>  osgi.bundles=org.eclipse.equinox.common@2:start, org.eclipse.update.configurator@3:start</pre>
+<p>When the Update configurator bundle starts, it automatically discovers and installs all the bundles in the <em>plugins</em>
+directory that is beside the Equinox JAR.  Note that the configurator does not automatically start these bundles.</p>
+<p>The steps above should get you running with a reasonably flexible configuration of Equinox and a collection of bundles.  
+Eclipse and Equinox offer a significant set of extensibilty and configuration options that are beyond the scope 
+of a getting started document (e.g., extension registry, application model, shared installs, ...).  
+To use the full power of Eclipse, look at how the Eclipse IDE and RCP applications are structures and 
+check out the Help system links above as well as</p> 
+<blockquote>
+  <p>
+    <a href="http://help.eclipse.org/help31/topic/org.eclipse.platform.doc.isv/reference/misc/multi_user_installs.html">http://help.eclipse.org/help31/topic/org.eclipse.platform.doc.isv/reference/misc/multi_user_installs.html</a>
+  </p>
+</blockquote>
+  <p>For those who are interested in some of the Eclipse extensions to OSGi, see</p>
 <blockquote>
   <p><a href="http://help.eclipse.org/help31/topic/org.eclipse.platform.doc.isv/reference/misc/bundle_manifest.html">http://help.eclipse.org/help31/topic/org.eclipse.platform.doc.isv/reference/misc/bundle_manifest.html</a></p>
 </blockquote>
-<p>Most of these facilities are experimental. Some general and we will propose
-      for OSGi R5, others are Eclipse-specific. All were put in place to solve
-      real problems that we (or our consumers) were having. You may be having
-related problems.</p>
+<p>Most of these facilities are experimental. Some general and have been proposed
+      for inclusion in the OSGi R4.1 or R5 specifications while others are Eclipse-specific. All were put in place to solve
+      real problems that we (or our consumers) were having. You may be having related problems.</p>
 
 <p class=bar>Other Information</p>
 <ul>
