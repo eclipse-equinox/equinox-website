699ec5387cbc4c69003879781b210a604b631391
dj <dj>
2007-02-26 19:19:12 +0000

    update doc

--- incubator/monitoring/doc.php
+++ incubator/monitoring/doc.php
@@ -28,30 +28,38 @@ ob_start();
 
 			<p class=bar>Terminology</p>
 			<ul>
-				<li>ContributionProvider: Class which extends the server plugins' org.eclipse.jmx.server.contribution extension point.</li>
-				<li>Contribution: Classes that are instantiated by a ContributionProvider. Instances of Contribution reside solely on the server 
+				<li><em>ContributionProvider</em>: Class which extends the server plugins' <code>org.eclipse.jmx.server.contribution</code> extension point.</li>
+				<li><em>Contribution</em>: Classes that are instantiated by a ContributionProvider. Instances of Contribution reside solely on the server 
 					and are registered with the server's mbean registry as dynamic mbeans.</li>
 			</ul>
 
 			<p class=bar>Server Side - Creating your own Contributor (Contribution Provider) plugin and Transport Layer</p>
-			Simple, create a plugin and extend the org.eclipse.equinox.jmx.server.contribution extension point.  Extending 
+			<p>
+			Simple, create a plugin and extend the <code>org.eclipse.equinox.jmx.server.contribution</code> extension point.  Extending 
 			this extension point requires you to define a class that is derived from ContributionProvider (more explanation 
 			about deriving from this class will follow) and optionally declaring any classes which this contribution provider 
-			knows how to 'extend'.</br></br>
+			knows how to 'extend'.
+			</p>
+			<p>
 			The methods of note when deriving from ContributionProvider are the providesType(Object o) and 
 			contributesType(Object o); together these methods define what objects (children) of itself and/or other 
-			contribution providers children (or contributions' children) it knows how to expose for remote management.</br></br>
+			contribution providers children (or contributions' children) it knows how to expose for remote management.
+			</p>
+			<p>
 			By returning true for providesType(Object o), it is assumed that createProvider(Object o) returns a 
 			ContributionProvider that has been specialized with the object.  By returning true for contributesType(Object o), 
 			it is assumed that the provider knows how to expose the particular object for remote management but has no 
-			interest in extending that object for its own purposes.</br></br>
+			interest in extending that object for its own purposes.
+			</p>
+			<p>
 			Pluggable transport providers provide a means for users to provide their own network transport layer 
 			implementation and can be utilized by extending the org.eclipse.equinox.jmx.server.provider extension point. 
 			 Currently, the project includes two transport plugins that users may use: apache xml-rpc (tested using jdk 1.4 
-			 on equinx foundation) or jdk5rmi (requires server to be running on jre 1.5+).  The protocol that the transports 
+			 on equinox foundation) or jdk5rmi (requires server to be running on jre 1.5+).  The protocol that the transports 
 			 use is what distinguishes them, and is a required field of the server's transport extension point.  By default 
-			 xmlrpc is used as a transport layer, this can be changed by setting the system property '-Dorg.eclipse.equinox.jmx.server.protocol=xxxx' 
+			 xmlrpc is used as a transport layer, this can be changed by setting the system property <code>-Dorg.eclipse.equinox.jmx.server.protocol=xxxx</code>
 			 prior to launching the application.
+			 </p>
 
 			<p class=bar>Client Side - Connecting and Viewing the Server's Exposed Resources</p>
 			The client plugin contributes a perspective that is used to connect and view a server's exposed resources 
