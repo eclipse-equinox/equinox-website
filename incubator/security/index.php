18af0eb7642ddf718e72e500af92c7af9ff893b9
mflaherty <mflaherty>
2007-07-19 20:02:10 +0000

    Added a mission statement

--- incubator/security/index.php
+++ incubator/security/index.php
@@ -1,62 +1,67 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 
-	#*****************************************************************************
-	#
-	#
-	#****************************************************************************
+   #*****************************************************************************
+   #
+   #
+   #****************************************************************************
 	
-	#
-	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "Equinox Incubator - Security";
-	$pageKeywords	= "equinox, incubator, security";
+   #
+   # Begin: page-specific settings.  Change these. 
+   $pageTitle 		= "Equinox Incubator - Security";
+   $pageKeywords	= "equinox, incubator, security";
 	
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+   # Add page-specific Nav bars here
+   # Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+   # $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+   # $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+   # $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
 
-	# End: page-specific settings
-	#
+   # End: page-specific settings
+   #
 		
-	# Paste your HTML content between the markers!	
-ob_start();
-?>		
+   # Paste your HTML content between the markers!	
+   ob_start();
+   ?>		
 
-	<div id="midcolumn">
-		<h1><?= $pageTitle ?></h1>
+   <div id="midcolumn">
 
-			<p class=bar>Mission Statement</p>
-<p>
-TODO
-</p>
+      <h1><?= $pageTitle ?></h1>
 
-			<p class=bar>Latest News</p>
-			<table width="100%" border="0">
-				<tr>
-					<td width="2%" align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
-					<td width="98%">May, 2006 - Initial release.</td>
-				</tr>
-			</table>
+      <p class=bar>Mission Statement</p>
+      <p>
+      To enable Java's standard security mechanisms within the Eclipse platform, and define new functionality
+      where there are gaps in the available standards. Extending Eclipse to support security will ensure that
+      mission critical applications will have the functionality needed to protect their data, and prevent against
+      malicious code masquerading as Eclipse plug-ins.
+      </p>
+
+      <p class=bar>Latest News</p>
+      <table width="100%" border="0">
+         <tr>
+            <td width="2%" align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+            <td width="98%">May, 2006 - Initial release.</td>
+         </tr>
+      </table>
 
 
-			<p class=bar>Active Investigators</p>
-			<ul>
-				<li>Matt Flaherty (IBM Lotus)</li>
-				<li>Eric W Li (IBM Lotus)</li>
-				<li>Ted Habeck (IBM Research)</li>
-			</ul>
-	</div>
+      <p class=bar>Active Investigators</p>
+      <ul>
+         <li>Matt Flaherty (IBM Lotus)</li>
+         <li>Eric W Li (IBM Lotus)</li>
+         <li>Ted Habeck (IBM Research)</li>
+      </ul>
+
+   </div>
 
 <?php
-	include $_SERVER['DOCUMENT_ROOT'] . "/equinox/global-links.html";
-	include $_SERVER['DOCUMENT_ROOT'] . "/equinox/incubator/component-links.html";
-	if (file_exists("dir-links.html")) {include "dir-links.html";}
+   include $_SERVER['DOCUMENT_ROOT'] . "/equinox/global-links.html";
+   include $_SERVER['DOCUMENT_ROOT'] . "/equinox/incubator/component-links.html";
+   if (file_exists("dir-links.html")) {include "dir-links.html";}
 ?>
 <?php
-	$html = ob_get_contents();
-	ob_end_clean();
+   $html = ob_get_contents();
+   ob_end_clean();
 
-	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+   # Generate the web page
+   $App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
