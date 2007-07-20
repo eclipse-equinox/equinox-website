3d69dcb78c0f00962640bacd849976c0b61bf5ca
mflaherty <mflaherty>
2007-07-20 16:13:27 +0000

    Started on goals

--- incubator/security/index.php
+++ incubator/security/index.php
@@ -146,7 +146,7 @@
       the following details:</p>
       
       <strong>Wiki:</strong> the security wiki category is available <a href="http://wiki.eclipse.org/Category:Security">here</a>.<br>
-      <strong>Bugs:</strong> prefixed with <font face="monospace">[security]</font>. A current list is <a href="">here</a>.<br>
+      <strong>Bugs:</strong> prefixed with <font face="monospace">[eq-sec]</font>. A current list is <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=substring&short_desc=%5Beq-sec%5D&classification=Eclipse&product=Equinox&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">here</a>.<br>
       <strong>CVS:</strong> branch name is <font face="monospace">security</font>
       
       
