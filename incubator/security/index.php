7cb19181922f12dcf694cc5621f4eda144f8946c
mflaherty <mflaherty>
2007-07-24 14:11:15 +0000

    Links links links

--- incubator/security/index.php
+++ incubator/security/index.php
@@ -12,8 +12,8 @@
 	
    # Add page-specific Nav bars here
    # Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-   # $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-   # $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+   $Nav->addNavSeparator("Security Links", 	"downloads.php");
+   $Nav->addCustomNav("A Link", "mypage.php", "_self", 3);
    # $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
 
    # End: page-specific settings
