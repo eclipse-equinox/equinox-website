909396ab1278833fd4bb993feb33778c3eb66b17
mflaherty <mflaherty>
2007-08-01 18:42:05 +0000

    Start work on 3.4 plan

--- incubator/security/plan.php
+++ incubator/security/plan.php
@@ -1,9 +1,9 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	#
 	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "Equinox Security 3.4 Plan";
-	$pageKeywords	= "text, editor, Java, plan, planning, JDT, jdt, development, tools, ide, Eclipse";
-	$pageAuthor		= "The Text Team";
+	$pageTitle 		= "DRAFT Equinox Security 3.4 Plan";
+	$pageKeywords	= "eclipse, equinox, security";
+	$pageAuthor		= "Equinox Security team";
 	
 	# Add page-specific Nav bars here
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
