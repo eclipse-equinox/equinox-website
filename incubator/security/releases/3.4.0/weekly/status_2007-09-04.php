f4a4d33786b86f0d8a72291fa5d6ed6b135c1b43
mflaherty <mflaherty>
2007-09-04 15:09:29 +0000

    *** empty log message ***

--- incubator/security/releases/3.4.0/weekly/status_2007-09-04.php
+++ incubator/security/releases/3.4.0/weekly/status_2007-09-04.php
@@ -7,7 +7,7 @@
 <li>Eric W Li</li>
 <li>Ted Habeck</li>
 </ul>
-
+<br>
 <strong>Updates:</strong>
 <ul>
 <li>Matt completed with <a href="http://www.eclipse.org/equinox/incubator/security/releases/3.4.0/update.php?target=M1">3.4 M1 update page</a>, and defined <a href="http://www.eclipse.org/equinox/incubator/security/plan.php#plan">3.4 M2 plan</a>.</li>
@@ -17,6 +17,6 @@ will be reusable for runtime (and SSL trust as well) (#199943).</li>
 <li>Ted has successfully posted a SWORD4J scan to the security site, and is working
 towards making it repeatable weekly.</li>
 </ul>
-
+<br>
 <strong>Other notes:</strong>
 Request to make JAAS work in an OSGi-only environment (remove extension dependency?): #202156
