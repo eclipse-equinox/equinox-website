212fa8366e4c2c3430f5adb3a9d664331624a931
mflaherty <mflaherty>
2007-09-04 14:55:17 +0000

    *** empty log message ***

---
+++ incubator/security/releases/3.4.0/weekly/status_2007-09-04.php
@@ -0,0 +1,18 @@
+<p class="bar">Tuesday Sept. 04, 2007
+</p>
+
+<strong>Meeting attendees:</strong> 
+Matt Flaherty
+Eric W Li
+Ted Habeck
+
+<strong>Updates:</strong>
+Matt completed with <a href="http://www.eclipse.org/equinox/incubator/security/releases/3.4.0/update.php?target=M1">3.4 M1 update page</a>, and defined <a href="http://www.eclipse.org/equinox/incubator/security/plan.php#plan">3.4 M2 plan</a>.
+Matt working on integrating the proxy jar into OSGi (#196988).
+Eric is creating a design doc for an API and prompt for load time security which
+will be reusable for runtime (and SSL trust as well) (#199943).
+Ted has successfully posted a SWORD4J scan to the security site, and is working
+towards making it repeatable weekly.
+
+Other notes:
+Request to make JAAS work in an OSGi-only environment (remove extension dependency?): #202156
