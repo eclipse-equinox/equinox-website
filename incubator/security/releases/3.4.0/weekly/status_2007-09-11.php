dacfa4382e12e40d08772a3499e5e692627bd5e8
mflaherty <mflaherty>
2007-09-11 16:28:35 +0000

    Update for 2007/09/11

---
+++ incubator/security/releases/3.4.0/weekly/status_2007-09-11.php
@@ -0,0 +1,33 @@
+<p class="bar">Tuesday Sept. 11, 2007
+</p>
+
+<p><strong>Meeting attendees:</strong>
+</p>
+<ul> 
+<li>Matt Flaherty</li>
+<li>Eric W Li</li>
+<li>Ted Habeck</li>
+<li>Oleg Besedin</li>
+<li>Tom Watson</li>
+</ul>
+
+<p><strong>Updates:</strong>
+</p>
+<ul>
+<li>Welcome to Oleg and Tom</li>
+<li>Discussion of security provider architecture, and why extclasspath changes (#22082) are needed</li>
+<li>Discussion of more complicated P2 provisioning use cases, and how they relate to trust management</li>
+<li>Code scanning update and potential enhancements to connect to ViewCVS</li>
+<li>Where can Oleg get involved with integrating contributions into Eclipse proper</li>
+</ul>
+
+<p><strong>Actions:</strong>
+</p>
+<ul>
+<li>Matt to start discussion on p2 security model going forward on equinox-dev</li>
+<li>Matt to finish testing of Tom's patch for (#22082)</li>
+<li>Matt to deliver changes to support extclasspath fragment for provider</li>
+<li>Eric to merge in changes in resolver code from 3.4 head to incubator copy of org.eclipse.osgi</li>
+<li>Ted to look into scan results linked to ViewCVS</li>
+<li>Oleg to consider integration points of JAAS code into eclipse. Startup sequence, getSubject() in context, etc</li>
+</ul>
