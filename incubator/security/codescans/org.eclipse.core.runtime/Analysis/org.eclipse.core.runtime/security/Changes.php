544fa37782562d7f063c916a209c63f199676da0
thabeck <thabeck>
2007-09-12 13:48:17 +0000

    Testing viewcvs integration.

--- incubator/security/codescans/org.eclipse.core.runtime/Analysis/org.eclipse.core.runtime/security/Changes.php
+++ incubator/security/codescans/org.eclipse.core.runtime/Analysis/org.eclipse.core.runtime/security/Changes.php
@@ -1,6 +1,31 @@
-<html>
-<title>SWORD4J analysis report generated Aug 29, 2007</title>
-<body><br/><br/>
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+   #*****************************************************************************
+   #
+   #
+   #****************************************************************************
+	
+   #
+   # Begin: page-specific settings.  Change these. 
+   $pageTitle 		= "Equinox Security - Code Scan Analysis";
+   $pageKeywords	= "equinox, incubator, security";
+	
+   # Add page-specific Nav bars here
+   # Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+   # $Nav->addNavSeparator("Security Links", 	"downloads.php");
+   # $Nav->addCustomNav("A Link", "mypage.php", "_self", 3);
+   # $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+   # End: page-specific settings
+   #
+		
+   # Paste your HTML content between the markers!	
+   ob_start();
+   ?>		
+
+   <div id="midcolumn">
+      <h1><?= $pageTitle ?></h1>
+      <p class=bar>Under construction...    Note:  Use cut/past from Plug-in reports into a Java Stack Trace console to navigate to source in your workspace. </p>
 <?php
  function open_file(projectName, packageName, fileName, lineNumber )
  {
@@ -30,8 +55,21 @@
   fclose($file);
  }
 ?>
+<title>SWORD4J analysis report generated Aug 29, 2007</title>
+<br/><br/>
 [sword4j] WARNING: in <a href="#" onClick='open_file(org.eclipse.core.runtime,org.eclipse.core.runtime,Preferences.java, 383 ")'> org.eclipse.core.runtime.Preferences.java:383</a>)
 <br/>[sword4j] void importPreferences(org.eclipse.core.runtime.IPath)
 <br/>[sword4j] J2SE Permission required to invoke the method.<br/>
-</body>
-</html>
+</div>
+
+<?php
+   include $_SERVER['DOCUMENT_ROOT'] . "/equinox/incubator/security/sidebar.html";
+?>
+<?php
+   $html = ob_get_contents();
+   ob_end_clean();
+
+   # Generate the web page
+   $App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
+
