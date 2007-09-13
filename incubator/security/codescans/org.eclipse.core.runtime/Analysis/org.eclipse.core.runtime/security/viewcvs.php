0c83b8b0a14633efe3e0bda67a882750fe837320
thabeck <thabeck>
2007-09-13 14:46:11 +0000

    Testing viewcvs integration.

--- incubator/security/codescans/org.eclipse.core.runtime/Analysis/org.eclipse.core.runtime/security/viewcvs.php
+++ incubator/security/codescans/org.eclipse.core.runtime/Analysis/org.eclipse.core.runtime/security/viewcvs.php
@@ -32,11 +32,11 @@
  {
    $int_line = 0 + $_REQUEST['linenumber'];
    echo "<br/>";
-   echo "int_line="+$int_line;
+   echo "int_line="+$int_line+"<br/>";
    $tmpPath = $_REQUEST['packageName']+"."+$_REQUEST['fileName'];
-   echo "tmpPath="+$tmpPath;
+   echo "tmpPath="+$tmpPath+"<br/>";
    $filePath = str_replace(".","/",$tmpPath);
-   echo "filePath="+$filePath;   
+   echo "filePath="+$filePath+"<br/>";
    $file = fopen("http://dev.eclipse.org/viewcvs/index.cgi/"+$_REQUEST['projectName']+"/src/"+$filePath+"?view=markup", "r");
    if (!$file) 
    {
