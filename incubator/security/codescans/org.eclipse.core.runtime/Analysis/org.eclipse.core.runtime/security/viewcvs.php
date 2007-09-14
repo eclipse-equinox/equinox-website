614ad3f0adf1ab6e6afd0cb9094dd8da359c914a
thabeck <thabeck>
2007-09-14 12:42:30 +0000

    Testing viewcvs integration.

--- incubator/security/codescans/org.eclipse.core.runtime/Analysis/org.eclipse.core.runtime/security/viewcvs.php
+++ incubator/security/codescans/org.eclipse.core.runtime/Analysis/org.eclipse.core.runtime/security/viewcvs.php
@@ -41,8 +41,6 @@
    echo "<br/>";
    $int_line = $_REQUEST['linenumber'];
    echo "int_line=$int_line<br/>";
-   $tmpPath = $_REQUEST['packageName'] . "." . $_REQUEST['fileName'];
-   echo "tmpPath=$tmpPath<br/>";
  }
  
  open_file();
