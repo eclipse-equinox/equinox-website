abfb55e74716689396dad75291564591cb3f7d26
mflaherty <mflaherty>
2007-10-08 02:19:33 +0000

    Add calendar link

--- incubator/security/status.php
+++ incubator/security/status.php
@@ -36,6 +36,8 @@
       </p>
       
       <p><strong>Call in details: (866) 880-0098 w/passcode 9639110</strong>
+      
+      <p><a target="_blank" href="http://www.google.com/calendar/event?action=TEMPLATE&amp;tmeid=NHQwODV0Nm1uNGNlMXR1bWVyOHJqZXEzZTRfMjAwNzEwMDlUMTQwMDAwWiA3dDRiZmJhb2Fia2hoc2FlanRzZTJzN2ltb0Bn&amp;tmsrc=N3Q0YmZiYW9hYmtoaHNhZWp0c2UyczdpbW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"><img border=0 src="http://www.google.com/calendar/images/ext/gc_button1_en.gif"></a></p>
       </p>
       
 <?php
