d52a2c02ce7f92743c7b44562ba71829c4fbc3e5
mflaherty <mflaherty>
2007-10-25 17:44:59 +0000

    Update for 2007/10/23

---
+++ incubator/security/releases/3.4.0/weekly/status_2007-10-23.php
@@ -0,0 +1,46 @@
+<p class="bar">Tuesday Oct. 23, 2007
+</p>
+
+<p><strong>Meeting attendees:</strong>
+</p>
+<ul> 
+<li>Pascal Rapicault (P2)</li>
+<li>Jeff McAffer (Equinox lead)</li>
+<li>Matt Flaherty</li>
+<li>Ted Habeck</li>
+<li>Tom Watson</li>
+<li>Oleg Besedin</li>
+<li>Eric W Li</li>
+</ul>
+
+<p><strong>Updates:</strong>
+</p>
+<ul>
+<li>Dove right into the P2 discussion.. Quick overview of needs:</li>
+<ul>
+<li>Verify signatures on content as it is downloaded</li>
+<li>UI for signature information about signed content</li>
+<li>UI for certificate details attached to a signature</li>
+<li>Prompt for trust when downloading signed content</li>
+<li>Secure connections to the repositories</li>
+<li>Prompt for passwords, etc when connecting to repositories</li>
+<li>Prompt for trust when making SSL connections to repositories</li>
+<li>UI for managing trusted certificates for SSL</li>
+<li>Protect platform from users not authorized to update (potentially NOT SecurityManager based)</li>
+<li>FUTURE: a way to model "trusted repositories" that will allow content to bypass ancillary signature checks</li>
+<li>FUTURE: a method to secure data (probably encrypt) is some storage area</li>
+<li>FUTURE: permissions for install related actions</li>
+</ul>
+</ul>
+
+<p><strong>Actions:</strong>
+</p>
+<ul>
+<li>Matt and Pascal to work detailed requirements into existing and new Bugzillas via equinox-dev</li>
+<li>Tom and Matt to look over .provisional verifier API and come up with a graduation plan.
+<li>Matt, Eric and Jay to produce high-level overview for security model by COB Monday</li>
+<li>Matt to invite Scott Lewis of ECF to a security meeting</li>
+<li>Matt to reach out to the UI designers w.r.t. prompting and management UI for alerts</li>
+</ul>
+<p>
+</p>
