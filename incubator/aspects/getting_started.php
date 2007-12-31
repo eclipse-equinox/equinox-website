190d5f1d031b052e0c4c16aeadd0eee17273f8fa
mlippert <mlippert>
2007-12-31 17:55:05 +0000

    download-zip now contains the feature description in the correct folder layout

--- incubator/aspects/getting_started.php
+++ incubator/aspects/getting_started.php
@@ -1,13 +1,30 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<title>Equinox OSGi incubator</title>
-<link rel="stylesheet" href="http://dev.eclipse.org/default_style.css" type="text/css">
-<link rel="stylesheet" href="../../equinox.css" type="text/css">
-</head>
-<body>
-<?php include("nav-include.html") ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Equinox Incubator - Aspects - Getting Started";
+	$pageKeywords	= "equinox, osgi, framework, runtime, incubator, aspects";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the markers!	
+ob_start();
+?>	
+<div id="midcolumn">
+<h1><?= $pageTitle ?></h1>
+
 <p class=bar>Getting Started</p>
 <p>
 <ol>
@@ -15,8 +32,8 @@
   <li>Install <a href="http://www.eclipse.org/ajdt/downloads/">AspectJ Development Tools (AJDT) 1.4.1</a>.</li>
   <li>Install the AspectJ Equinox Framework Extension using
   either the update site <code>http://download.eclipse.org/tools/aspectj/dev/update</code> or by
-  downloading and unzipping <a href="http://download.eclipse.org/tools/aspectj/dev/update/org.aspectj.osgi_1.0.3_archive.zip">
-  <code>org.aspectj.osgi_1.0.3_archive.zip</code></a>.</li>
+  downloading and unzipping <a href="downloads/org.aspectj.osgi_1.0.3a_archive.zip">
+  <code>org.aspectj.osgi_1.0.3a_archive.zip</code></a>.</li>
 <!--  
   <li>Check out the org.aspectj.osgi, org.aspectj.osgi.service.weaving and org.eclipse.osgi_3.2.0 bundles
   from the "aspects" folder in the <a href="http://www.eclipse.org/equinox/incubator/howto.php">incubator repository</a>
@@ -58,5 +75,21 @@ then make sure you are using the config.ini provided. To control the amount of
 diagnostic information use the various system properties declared in that file.
 You can also use the diagnostic configuration provided by 
 <a href="http://www.eclipse.org/aspectj/doc/released/devguide/ltw-configuration.html#weaver-options">AspectJ LTW</a>.
-</body>
-</html>
+
+<p>&nbsp;</p>
+
+</div>
+
+<?php
+	include $_SERVER['DOCUMENT_ROOT'] . "/equinox/global-links.html";
+	include $_SERVER['DOCUMENT_ROOT'] . "/equinox/incubator/component-links.html";
+	if (file_exists("dir-links.html")) {include "dir-links.html";}
+?>
+
+<?php
+	$html = ob_get_contents();
+	ob_end_clean();
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
