91540fdfa392904ccabf85865ae623a1b72ecbd2
mlippert <mlippert>
2008-11-10 19:30:12 +0000

    M3 milestone and new dev build 200811012141

--- incubator/aspects/index.php
+++ incubator/aspects/index.php
@@ -28,7 +28,13 @@ ob_start();
 <p class=bar>Latest news</p>
 
 <p>
-  <b>Sep 19 2008</b> - <img src="http://www.eclipse.org/images/new.gif" width="31" height="14" />
+  <b>Oct 31 2008</b> - <img src="http://www.eclipse.org/images/new.gif" width="31" height="14" />
+  Milestone 3 for Equinox Aspects 1.0 is there! You can get it at the
+  <a href="equinox-aspects-downloads.php">Downloads</a>.
+</p>
+
+<p>
+  <b>Sep 19 2008</b> - 
   Milestone 2 for Equinox Aspects 1.0 is there! You can get it at the
   <a href="equinox-aspects-downloads.php">Downloads</a>.
 </p>
