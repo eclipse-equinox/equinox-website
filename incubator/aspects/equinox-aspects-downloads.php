d436f71be84e6372e89b2f135c15e10c995797b8
mlippert <mlippert>
2009-02-16 21:57:40 +0000

    new dev build 200902162254

--- incubator/aspects/equinox-aspects-downloads.php
+++ incubator/aspects/equinox-aspects-downloads.php
@@ -92,6 +92,13 @@ ob_start();
 
 <tr>
 	<td width="2%" align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
+	<td width="98%"><a href="downloads/dev/org.eclipse.equinox.weaving-incubation-1.0.0.200902162254-archive.zip">
+	    org.eclipse.equinox.weaving-incubation-1.0.0.200902162254-archive.zip</a>
+    </td>
+</tr>
+
+<tr>
+	<td width="2%" align=RIGHT valign=TOP><img src="http://eclipse.org/images/Adarrow.gif" width="16" height="16" border="0"></td>
 	<td width="98%"><a href="downloads/dev/org.eclipse.equinox.weaving-incubation-1.0.0.200901301237-archive.zip">
 	    org.eclipse.equinox.weaving-incubation-1.0.0.200901301237-archive.zip</a>
     </td>
