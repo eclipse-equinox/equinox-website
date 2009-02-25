c6e00237f85e8b8db006ef21b78698716c6992f6
mlippert <mlippert>
2009-02-25 19:22:14 +0000

    hm, strange look, trying to find out what is going wrong

---
+++ incubator/aspects/_projectCommon.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<style type="text/css">
+	.inset {
+		border:solid 1px #ccc;
+		background: #eee;
+		padding: 0.3em;
+		margin-left: 2.5em;
+		margin-right: 2.5em;
+	}
+	
+	li .inset {
+		margin-left: 0;
+	}
+	
+	#midcolumn li {
+		margin-bottom: 0.5em;
+	}
+</style>
