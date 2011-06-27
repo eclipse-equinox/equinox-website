d6cec443b0dc5dff013b353a7ac37019c76ab7a8
twatson <twatson>
2011-06-27 14:07:54 +0000

    *** empty log message ***

--- planning/index.php
+++ planning/index.php
@@ -11,12 +11,14 @@
 			<ul>
 	      <li><a href="http://www.eclipse.org/projects/project-plan.php?projectid=rt.equinox">Current plan</a><br>
 	        This document lays out the feature and API set for the current planned release of the Equinox Project. </li>
-	      <li><a href="freeze_plan_3.7.php"> Equinox Project Indigo Endgame Plan</a><br>
-	    		Information about how we plan to wrap up the Indigo release (version 3.7.0).</li> 
 	    </ul>
       	
 			<h3>Historical information about past releases</h3>			
 		<ul>
+		  <li><a href="equinox_indigo_plan.xml"> Equinox Project Indigo Plan</a><br>
+	        This document lays out the feature and API set for the Indigo (3.7) release of the Equinox Project. </li>
+	      <li><a href="freeze_plan_3.7.php"> Equinox Project Indigo Endgame Plan</a><br>
+	    	Information about how we plan to wrap up the Indigo release (version 3.7.0).</li> 
 		  <li><a href="equinox_helios_plan.xml"> Equinox Project Helios Plan</a><br>
 	        This document lays out the feature and API set for the Helios (3.6) release of the Equinox Project. </li>
 	      <li><a href="freeze_plan_3.6.php"> Equinox Project Helios Endgame Plan</a><br>
