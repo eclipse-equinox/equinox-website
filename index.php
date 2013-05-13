fb194c0ec06e6a52d240c6ede2ab6d3fdf541b80
Thomas Watson <tjwatson@us.ibm.com>
2013-05-13 13:28:48 -0500

    Do not hard code in the version Equinox implements

--- index.php
+++ index.php
@@ -48,17 +48,21 @@ $html=<<<EOHTML
 <div id="midcolumn">
 	<div class="homeitem">
 		<h3>Mission Statement</h3>
-		<p>From a code point of view, Equinox is an implementation of the <a href="http://osgi.org/osgi_technology/download_specs.asp?section=2#Release4">OSGi
-			R4 core framework specification</a>, a set of bundles that implement
+		<p>From a code point of view, Equinox is an implementation of the <a href="http://www.osgi.org/Specifications/HomePage">OSGi
+			core framework specification</a>, a set of bundles that implement
 			various optional OSGi services and other infrastructure for running OSGi-based
-			systems.
+			systems.  The Equinox OSGi core framework implementation is used as the 
+			reference implementation and as such it implements all the required features of
+			the latest OSGi core framework specification.
 		</p><p>More generally, the goal of the Equinox project is to be a first class OSGi
 			community and foster the vision of Eclipse as a landscape of bundles. As
 			part of this, it is responsible for developing and delivering the OSGi framework
 			implementation used for all of Eclipse. In addition. the project is open to:
 		</p>
 		<ul>
-			<li>Implementation of all aspects of the OSGi specification (including the EEG, MEG and VEG work) </li>
+			<li>Implementation of all aspects of the OSGi specification.  The main focus is specifications
+			    maintained by the Core Platform Expert Group (CPEG) but also may include implementations
+			    specified by other expert groups such as the Enterprise Expert Group (EEG)</li>
 			<li> Investigation and research related to future versions of OSGi specifications and related runtime issues </li>
 			<li>Development of non-standard infrastructure deemed to be essential
 				to the running and management of OSGi-based systems </li>
